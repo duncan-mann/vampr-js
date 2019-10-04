663af50f16b620c0cc8177c2308c3380707ce5e5
Duncan Mann <duncan.s.mann@gmail.com>
2019-10-04 16:56:55 +0000

    Completed methods 1-4, however Test 4 will not run in my terminal.

--- vampire.js
+++ vampire.js
@@ -10,22 +10,30 @@ class Vampire {
 
   // Adds the vampire as an offspring of this vampire
   addOffspring(vampire) {
-
+    this.offspring.push(vampire);
+    vampire.creator = this;
   }
 
   // Returns the total number of vampires created by that vampire
   get numberOfOffspring() {
-
+    return this.offspring.length;
   }
 
   // Returns the number of vampires away from the original vampire this vampire is
   get numberOfVampiresFromOriginal() {
-
+    let numCreators = 0;
+    let currentVampire = this;
+    while (currentVampire.creator) {
+      currentVampire = this.creator;
+      numCreators++
+    }
+    return numCreators;
   }
 
   // Returns true if this vampire is more senior than the other vampire. (Who is closer to the original vampire)
   isMoreSeniorThan(vampire) {
-
+    let answer = this.numberOfVampiresFromOriginal < vampire.numberOfVampiresFromOriginal; 
+    return answer;
   }
 
   /** Stretch **/
